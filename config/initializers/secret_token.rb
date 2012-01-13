7ed2fd5259e6b66a4d0cfc7060d190df481f1ec0
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-01-12 18:20:42 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geo::Application.config.secret_token = '0747a1f022c326dd5c0b5066ea8bc58eddd11ae36bf77bcf7e9e4ec9f7ed5c2e58f5ae0a580743cb15afa3e781b0c646936d1d2bc02fddd19f9062fb3da7ca4e'
